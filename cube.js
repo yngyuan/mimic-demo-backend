e185e69a96b809d433f975de18fd8d2ab6ed24ad
yngyuan <zedd.yuan@utexas.edu>
2021-03-03 00:32:20 -0600

    added auth

--- cube.js
+++ cube.js
@@ -1,3 +1,14 @@
-// Cube.js configuration options: https://cube.dev/docs/config
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const jwkToPem = require("jwk-to-pem");
+const jwks = JSON.parse(fs.readFileSync("jwks.json"));
+const _ = require("lodash");
+
 module.exports = {
+    checkAuth: async (req, auth) => {
+        const decoded = jwt.decode(auth, { complete: true });
+        const jwk = _.find(jwks.keys, x => x.kid === decoded.header.kid);
+        const pem = jwkToPem(jwk);
+        req.securityContext = jwt.verify(auth, pem);
+    }
 };
